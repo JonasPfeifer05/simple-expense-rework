2af394f4f0a79b41473652536fc13fc095802804
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-05-03 08:33:29 +0200

    init docker for client

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>Hello World</h1>
 </template>
+
+<style scoped>
+
+</style>
