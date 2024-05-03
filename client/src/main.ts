90c79368299f4f6f5922ff28ef28daeed2482319
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-05-03 09:45:43 +0200

    added login page

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,17 @@
-import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { globalCookiesConfig } from "vue3-cookies";
 
 import App from './App.vue'
 import router from './router'
+
+
+globalCookiesConfig({
+    expireTimes: "30d",
+    secure: true,
+});
 
 const app = createApp(App)
 
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from "@/views/LoginView.vue";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
@@ -6,9 +7,14 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginView
     },
+    {
+      path: "/home",
+      name: "home",
+      component: HomeView
+    }
   ]
 })
 
