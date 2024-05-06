6560e26f12d09c6e6f2731b13c5bbf1b3165a508
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-05-06 11:40:32 +0200

    added nuxt client

---
+++ client-nuxt/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+console.log(process.env.BACKEND_HOST)
+export default defineNuxtConfig({
+    devtools: {enabled: true},
+    ssr: true,
+    modules: ['@nuxtjs/color-mode', '@nuxtjs/google-fonts', 'nuxt-icon', '@pinia/nuxt', "nuxt-security"],
+    routeRules: {
+        '/api/**': {
+            proxy: {to: `${process.env.BACKEND_HOST ?? "http://localhost"}:${process.env.BACKEND_PORT ?? "3001"}/api/**`,},
+        }
+    },
+    googleFonts: {
+        download: true,
+        families: {
+            Roboto: true,
+        }
+    },
+    css: ["~/assets/main.scss", "~/assets/light-mode.scss", "~/assets/dark-mode.scss"],
+
+})
